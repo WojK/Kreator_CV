696897db54fb95e99eb901a5a59fbf5ed8a85ccf
Kuba072002 <jakub072002@gmail.com>
2023-03-07 23:04:38 +0100

    Responsive properties for navbar and footer

--- cvclient/src/components/navbar/Navbar.jsx
+++ cvclient/src/components/navbar/Navbar.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { AiFillCaretDown } from "react-icons/ai";
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './navbar.css'
 
+const Menu = () => (
+    <>
+        <p><a href="#home"><AiFillCaretDown color="#fff" size={20} />Creator</a></p>
+        <p><a href="#home"><AiFillCaretDown color="#fff" size={20} />Advices</a></p>
+        <p><a href="#home"><AiFillCaretDown color="#fff" size={20} />About us</a></p>
+    </>
+);
+
 const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
+
     return (
         <div className='cv__navbar'>
             <div className='cv__navbar-logo'>
@@ -10,14 +21,32 @@ const Navbar = () => {
             </div>
             <div className='cv__navbar-links'>
                 <div className='cv__navbar-links_container'>
-                    <p><AiFillCaretDown color="#fff" size={20} /><a href="#home">Creator</a></p>
-                    <p><AiFillCaretDown color="#fff" size={20} /><a href="#home">Advices</a></p>
-                    <p><AiFillCaretDown color="#fff" size={20} /><a href="#home">About us</a></p>
+                    <Menu />
                 </div>
 
                 <div className='cv__navbar-account'>
                     <button type='button'>My account</button>
                 </div>
+            </div>
+
+            <div className='cv__navbar_menu'>
+                {toggleMenu
+                    ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+                    : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />
+                }
+                {toggleMenu && (
+                    <div className='cv__navbar_menu_container scale-up-center'>
+
+                        <div className='cv__navbar_menu_container-links'>
+                            <Menu />
+                        </div>
+
+                        <div className="cv__navbar_menu_container-account">
+                            <button type='button'>My account</button>
+                        </div>
+
+                    </div>
+                )}
             </div>
         </div>
     )
