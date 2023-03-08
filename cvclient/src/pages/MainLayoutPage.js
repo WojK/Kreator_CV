e7bf8e2e23c58f3135e4e5b4e6efa1fb00d78a30
MichalZdanuk <zdanuk_m@wp.pl>
2023-03-08 21:41:29 +0100

    small fix

--- cvclient/src/pages/MainLayoutPage.js
+++ cvclient/src/pages/MainLayoutPage.js
@@ -2,15 +2,17 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import { Footer, Navbar, Login, Register } from "../components";
 import classes from "./MainLayoutPage.module.css";
+import { useLocation } from "react-router-dom";
 
 const MainLayoutPage = () => {
+  const location = useLocation();
   return (
     <>
       <div className={classes["div-content"]}>
         <Navbar />
         <Outlet />
-        <Login />
-        <Register />
+        {location.pathname === "/my_account" ? <Login /> : ""}
+        {location.pathname === "/my_account" ? <Register /> : ""}
       </div>
       <Footer />
     </>
