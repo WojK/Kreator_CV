f9af2c21ac0c9f77b66957995fcd34cdac6c1bca
smoleckk <ksmolecki123@wp.pl>
2023-04-14 19:12:15 +0200

    Fix image slider

--- cvclient/src/pages/ChooseTemplateStylesPage.jsx
+++ cvclient/src/pages/ChooseTemplateStylesPage.jsx
@@ -1,12 +1,12 @@
 import classes from "./ChooseTemplateStylesPage.module.css";
-import { motion } from 'framer-motion'
 import { useRef, useEffect, useState } from "react";
 import image1 from "../assets/images/img1.jpg"
 import image2 from "../assets/images/img2.jpg"
 import image3 from "../assets/images/img3.jpg"
 import image4 from "../assets/images/img4.jpg"
+import ImageSlider from "../components/imageSlider/ImageSlider"
 const ChooseTemplateStylesPage = () => {
-  const data_templates = [
+  const slides = [
     {
       image: image1,
       link: "/"
@@ -25,32 +25,12 @@ const ChooseTemplateStylesPage = () => {
     },
   ];
 
-  const [width, setWidth] = useState(0);
-  const carousel = useRef();
-
-  useEffect(() => {
-    console.log(carousel.current);
-    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth)
-  }, []);
-
-  return (<div className={classes["templates-div"]}>
-    <motion.div ref={carousel} className={classes["carousel"]} whileTap={{ cursor: "grabbing" }}>
-      <motion.div drag="x" dragConstraints={{ right: 0, left: -width }} className={classes["inner-carousel"]} >
-        {data_templates.map(template => {
-          return (
-            <motion.div className={classes["item"]} key={template}>
-              <div className={classes["item-inside"]}>
-                <img src={template.image} alt="" />
-                <a className={classes["button"]} href={template.link}>
-                  Lets build your future!
-                </a>
-              </div>
-            </motion.div>
-          );
-        })}
-      </motion.div>
-    </motion.div>
-  </div>
+  return (
+    <div>
+      <div className={classes["containerStyles"]}>
+        <ImageSlider slides={slides} />
+      </div>
+    </div>
   );
 };
 
